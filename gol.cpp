a98c42077811b144be7dc987768ad205c52fd261
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2023-11-09 17:06:16 +0530

    Updated the Code to a better Cache-Friendly version

--- gol.cpp
+++ gol.cpp
@@ -1,4 +1,4 @@
-// TO-DO :: More Cache-Friendly Code
+// TO-DO :: More Cache-Friendly Code :::::::::::::::::::::::::::::::::::::::::: DONE
 // TO-DO :: Make A Program To Implement This On Terminal
 
 #include "screen.h"
@@ -9,43 +9,40 @@ const int SCREEN_WIDTH = 256;  // Define RENDER SCREEN WIDTH (Don't Keep It
                                // Beyond 256, Otherwise It Will Be Glitchy)
 
 // isAlive Function To Determine The FATE Of The Next Geenration
-bool isAlive(std::array<std::array<int, SCREEN_HEIGHT>, SCREEN_WIDTH> arr,
-             int a, int b) {
+bool isAlive(std::array<std::array<int, SCREEN_HEIGHT>, SCREEN_WIDTH> arr, int a, int b) {
   // Conway's Game Of Life Rules :-
-  // Any live cell with fewer than two live neighbours dies, as if by
-  // underpopulation. Any live cell with two or three live neighbours lives on
-  // to the next generation. Any live cell with more than three live neighbours
-  // dies, as if by overpopulation. Any dead cell with exactly three live
-  // neighbours becomes a live cell, as if by reproduction.
+  	// Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+  	// Any live cell with two or three live neighbours lives on to the next generation. 
+  	// Any live cell with more than three live neighbours dies, as if by overpopulation. 
+  	// Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 
   // Shortened/Concise Rules
-  // Any live cell with two or three live neighbours survives.
-  // Any dead cell with three live neighbours becomes a live cell.
-  // All other live cells die in the next generation. Similarly, all other dead
-  // cells stay dead.
+  	// Any live cell with two or three live neighbours survives.
+  	// Any dead cell with three live neighbours becomes a live cell.
+	// All other live cells die in the next generation. Similarly, all other dead cells stay dead.
 
   int aliveNeighbours = 0; // Count the number of alive neighbours of the point
+
+  if ((a > 0 && b > 0) && arr[a - 1][b - 1] == 1)
+    aliveNeighbours++; // Checking For Top-Left Neighbours
+  if (a > 0 && arr[a - 1][b] == 1)
+    aliveNeighbours++; // Checking For Top Neighbours
+  if ((a > 0 && b < SCREEN_WIDTH) && arr[a - 1][b + 1] == 1)
+    aliveNeighbours++; // Checking For Top-Right Neighbours
 
   if (b > 0 && arr[a][b - 1] == 1)
     aliveNeighbours++; // Checking For Left Neighbours
   if (b < SCREEN_WIDTH && arr[a][b + 1] == 1)
     aliveNeighbours++; // Checking For Right Neighbours
-  if (a > 0 && arr[a - 1][b] == 1)
-    aliveNeighbours++; // Checking For Top Neighbours
-  if (a < SCREEN_HEIGHT && arr[a + 1][b] == 1)
-    aliveNeighbours++; // Checking For Bottom Neighbours
-
-  if ((a > 0 && b > 0) && arr[a - 1][b - 1] == 1)
-    aliveNeighbours++; // Checking For Top-Left Neighbours
+  
   if ((a < SCREEN_HEIGHT && b > 0) && arr[a + 1][b - 1] == 1)
     aliveNeighbours++; // Checking For Bottom-Left Neighbours
-  if ((a > 0 && b < SCREEN_WIDTH) && arr[a - 1][b + 1] == 1)
-    aliveNeighbours++; // Checking For Top-Right Neighbours
+  if (a < SCREEN_HEIGHT && arr[a + 1][b] == 1)
+    aliveNeighbours++; // Checking For Bottom Neighbours
   if ((a < SCREEN_HEIGHT && b < SCREEN_WIDTH) && arr[a + 1][b + 1] == 1)
     aliveNeighbours++; // Checking For Bottom-Right Neighbours
 
-  // POINT'S FATE For The Next Generation (Alive OR Dead) <Alive :: True, Dead
-  // :: False>
+  // POINT'S FATE For The Next Generation (Alive OR Dead) <Alive :: True, Dead :: False>
   if (arr[a][b] == 1 && (aliveNeighbours == 2 || aliveNeighbours == 3))
     return true;
   if (arr[a][b] == 0 && (aliveNeighbours == 3))
@@ -66,8 +63,7 @@ int main() {
 
   // Generate Random Alive Points At The Beginning
   for (auto &row : mainDisp)
-    std::generate(row.begin(), row.end(),
-                  []() { return rand() % 10 == 0 ? 1 : 0; });
+    std::generate(row.begin(), row.end(), []() { return rand() % 10 == 0 ? 1 : 0; });
 
   // Render The World For "Game Of Life" Show
   while (true) {
@@ -90,9 +86,8 @@ int main() {
     // Replace Buffer Display With Main Display For The Next Generation
     std::copy(bufferDisp.begin(), bufferDisp.end(), mainDisp.begin());
 
-    // Do All The Necessary Operatios To Render, Plot/Draw & Clear Screen (For
-    // Next Generation) Note That All Are Wrapper Function For SDL2 Library
-    // Functions
+    // Do All The Necessary Operatios To Render, Plot/Draw & Clear Screen (For Next Generation) 
+    // Note That All Are Wrapper Function For SDL2 Library Functions
     screen.update();
     SDL_Delay(25);
     screen.input();
